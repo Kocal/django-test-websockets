18899cf7c57b91cb8987304af0bd847afe90b10b
Hugo ALLIAUME <kocal@live.fr>
2016-04-20 11:07:56 +0200

    Add simple example

--- DjangoTestWebsockets/settings.py
+++ DjangoTestWebsockets/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'DjangoTestWebsockets.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def index(request):
-
-    return HttpResponse('toast')
+    return render(request, 'myapp/index.html')
