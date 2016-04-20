221d0184eae7a4a62796ebe0205ca9f1c97a4269
Hugo ALLIAUME <kocal@live.fr>
2016-04-20 16:19:07 +0200

    Integration of official example of ws4redis websocket chat

--- DjangoTestWebsockets/settings.py
+++ DjangoTestWebsockets/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -120,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/');
 
 # ws4redis
 WEBSOCKET_URL = '/ws/'
+WS4REDIS_HEARTBEAT = '--heartbeat--'
